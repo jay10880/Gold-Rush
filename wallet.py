8532087fa9783c5199ff99051eded03243226d6d
Jay Patel <thebat@Jays-MBP.lan1>
2019-09-03 20:35:22 -0700

    made some changes in proof of work

--- wallet.py
+++ wallet.py
@@ -14,13 +14,11 @@ class Wallet:
         self.node_id = node_id
 
     def create_keys(self):
-        """Create a new pair of private and public keys."""
         private_key, public_key = self.generate_keys()
         self.private_key = private_key
         self.public_key = public_key
 
     def save_keys(self):
-        """Saves the keys to a file (wallet.txt)."""
         if self.public_key != None and self.private_key != None:
             try:
                 with open('wallet-{}.txt'.format(self.node_id), mode='w') as f:
@@ -33,7 +31,6 @@ class Wallet:
                 return False
 
     def load_keys(self):
-        """Loads the keys from the wallet.txt file into memory."""
         try:
             with open('wallet-{}.txt'.format(self.node_id), mode='r') as f:
                 keys = f.readlines()
@@ -47,19 +44,11 @@ class Wallet:
             return False
 
     def generate_keys(self):
-        """Generate a new pair of private and public key."""
         private_key = RSA.generate(1024, Crypto.Random.new().read)
         public_key = private_key.publickey()
         return (binascii.hexlify(private_key.exportKey(format='DER')).decode('ascii'), binascii.hexlify(public_key.exportKey(format='DER')).decode('ascii'))
 
     def sign_transaction(self, sender, recipient, amount):
-        """Sign a transaction and return the signature.
-
-        Arguments:
-            :sender: The sender of the transaction.
-            :recipient: The recipient of the transaction.
-            :amount: The amount of the transaction.
-        """
         signer = PKCS1_v1_5.new(RSA.importKey(binascii.unhexlify(self.private_key)))
         h = SHA256.new((str(sender) + str(recipient) + str(amount)).encode('utf8'))
         signature = signer.sign(h)
@@ -67,11 +56,6 @@ class Wallet:
 
     @staticmethod
     def verify_transaction(transaction):
-        """Verify the signature of a transaction.
-
-        Arguments:
-            :transaction: The transaction that should be verified.
-        """
         public_key = RSA.importKey(binascii.unhexlify(transaction.sender))
         verifier = PKCS1_v1_5.new(public_key)
         h = SHA256.new((str(transaction.sender) + str(transaction.recipient) + str(transaction.amount)).encode('utf8'))
